629cd0243c1da7b4e3a08cdd28a3a5ccf29b5455
Julian <julian@contingent.dk>
2016-09-02 16:03:11 +0200

    hax solution for splitting lat, lng to decimal numbers

--- record.js
+++ record.js
@@ -17,14 +17,29 @@ Record.prototype.hydrate = function (values) {
 Record.prototype.fromWeb3Array = function (array) {
     this.owner = array[0]
     this.time = array[1]
-    this.lat = array[2]
-    this.lng = array[3]
+
+    this.lat = Record.splitInt(array[2], 3)
+    this.lng = Record.splitInt(array[3], 3)
+
     this.unregisterCost = array[4]
     this.key = array[5]
+
+    this._debug_fromWeb3Array = array
 }
 
 Record.prototype.toString = function () {
     return JSON.stringify(this)
+}
+
+// input: "1267", 2 output: 67.12
+Record.splitInt = function (int, precision) {
+    if (typeof int !== "string")
+        int = int.toString();
+
+    if (int.length > precision)
+        int = int.substring(int.length-precision, int.length ) + "." + int.substring(0, int.length-precision);
+
+    return int;
 }
 
 module.exports = Record
